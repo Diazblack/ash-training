e2a5be91ce78fda5b9184bb22de1fe59aeb3e99b
Cesar Jolibois <diazcd85@gmail.com>
2025-05-08 10:19:27 -0400

    Completed lab 0

---
+++ lib/twitter/tweets/tweet.ex
@@ -0,0 +1,18 @@
+defmodule Twitter.Tweets.Tweet do
+  use Ash.Resource, otp_app: :twitter, domain: Twitter.Tweets, data_layer: AshPostgres.DataLayer
+
+  actions do
+    defaults [:read, :destroy]
+  end
+
+  attributes do
+    uuid_primary_key :id
+
+    timestamps()
+  end
+
+  postgres do
+    table "tweets"
+    repo Twitter.Repo
+  end
+end
